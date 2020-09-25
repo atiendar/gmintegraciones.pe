bff645bf7f4527ceecf947c1eaea9cf1924967bb
Aaron Sánchez <desarrolloweb.ewmx@gmail.com>
2020-09-25 12:42:25 -0500

    Correccion de error en las seed

--- database/factories/PedidoArmadoTieneDireccionFactory.php
+++ database/factories/PedidoArmadoTieneDireccionFactory.php
@@ -11,10 +11,11 @@ use Faker\Generator as Faker;
 
 $factory->define(PedidoArmadoTieneDireccion::class, function (Faker $faker) {
   $usuario            = $faker->randomElement(User::where('acceso', '1')->pluck('email_registro'));
-  $armado             = $faker->randomElement(PedidoArmado::pluck('id'));
+  $armado             = $faker->randomElement(PedidoArmado::get(['id', 'cod']));
   $metodo_de_entrega  = $faker->randomElement(MetodoDeEntrega::pluck('nom_met_ent'));
-  $estado    = $faker->randomElement(Estado::pluck('est'));
+  $estado             = $faker->randomElement(Estado::pluck('est'));
   return [
+    'cod'                       => $armado->cod.'-'.$faker->stateAbbr,
     'cant'                      => $faker->randomElement([1,2,3,4]),
     'estat'                     => $faker->randomElement([config('app.pendiente'), config('app.en_almacen_de_salida')]),
     'met_de_entreg'             => $metodo_de_entrega,
@@ -22,7 +23,7 @@ $factory->define(PedidoArmadoTieneDireccion::class, function (Faker $faker) {
     'for_loc'                   => $faker->randomElement(config('opcionesSelect.select_foraneo_local')),
     'detalles_de_la_ubicacion'  => $faker->paragraph,
     'tip_env'                   => $faker->randomElement(config('opcionesSelect.select_tipo_de_envio_plus')),
-    'pedido_armado_id'          => $armado,
+    'pedido_armado_id'          => $armado->id,
     'created_at_direc_arm'      => $usuario,
   ];
 });
