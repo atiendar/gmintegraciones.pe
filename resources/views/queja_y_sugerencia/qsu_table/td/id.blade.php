3ae9d2d7afe09f4a6ea916261c012b75aeebe156
Aaron Sánchez <desarrolloweb.ewmx@gmail.com>
2020-09-30 19:36:05 -0500

    Mejora en la tabla de quejas y sugerencias

--- resources/views/queja_y_sugerencia/qsu_table/td/id.blade.php
+++ resources/views/queja_y_sugerencia/qsu_table/td/id.blade.php
@@ -1,4 +1,4 @@
-<td>
+<td width="1rem">
   @can('qys.show')
     <a href="{{ route('qys.show', Crypt::encrypt($queja_y_sugerencia->id)) }}" title="Detalles: {{ $queja_y_sugerencia->id }}">{{ $queja_y_sugerencia->id }}</a>
   @else
