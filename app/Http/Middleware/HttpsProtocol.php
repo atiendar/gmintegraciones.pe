de785e0bc186da93a9a32480cbe145d50371f563
Aaron Sánchez <desarrolloweb.ewmx@gmail.com>
2020-10-15 13:01:15 -0500

    quitar https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -7,7 +7,7 @@ class HttpsProtocol {
     if(env('APP_ENV') == 'production') {
       // Redirecciona al https
       if (!$request->secure()) {
-        return redirect()->secure($request->getRequestUri());
+        // return redirect()->secure($request->getRequestUri());
       }
     }
     return $next($request);
