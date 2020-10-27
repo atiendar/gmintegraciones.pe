9dd030c7bddc42da39651a3b633e2d1f97329994
AaroOnSancheZz <aaronsanchez269@gmail.com>
2020-10-27 10:34:45 -0600

    Prueba al generar reporte de compra 5

--- app/Exports/almacen/producto/generarReporteDeCompraExport.php
+++ app/Exports/almacen/producto/generarReporteDeCompraExport.php
@@ -11,7 +11,7 @@ use App\Models\Producto;
 class generarReporteDeCompraExport implements FromView {
     use Exportable;
     public function view(): View {
-
+/*
     $sss =  Producto::with(['sustitutos', 'productos_pedido' => function($query) {
         $query->with(['armado' => function($query) {
             $query->where('estat', config('app.pendiente'))->orWhere('estat', config('app.en_espera_de_compra'))->orWhere('estat', config('app.en_revision_de_productos'));
@@ -19,7 +19,7 @@ class generarReporteDeCompraExport implements FromView {
     }])->where('id', 2)->first();
     
     dd(   $sss    );
-
+*/
         return view('almacen.producto.exports.alm_pro_exp_generarReporteDeCompra', [
             'productos' => Producto::with(['sustitutos', 'productos_pedido' => function($query) {
                 $query->with(['armado' => function($query) {
