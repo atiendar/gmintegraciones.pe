9138a45c3a5acc94fc8c6667b6e860183beef03a
Aaron Sánchez <desarrolloweb.ewmx@gmail.com>
2020-11-12 13:00:37 -0600

    error

--- resources/views/rolFerro/envio/foraneo/efor_index.blade.php
+++ resources/views/rolFerro/envio/foraneo/efor_index.blade.php
@@ -12,14 +12,6 @@
       @include('global.buscador.buscador', ['ruta_recarga' => route('rolFerro.envioForaneo.index'), 'opciones_buscador' => config('opcionesSelect.select_logistica_direcciones_index')])
     {!! Form::close() !!}
     @include('rolFerro.envio.local.eloc_table')
-    <div class="row">
-      <div class="form-group col-sm btn-sm" >
-        <a href="{{ route('rolFerro.envioForaneo.index') }}" class="btn btn-default w-50 p-2 border"><i class="fas fa-sign-out-alt text-dark"></i> {{ __('Regresar') }}</a>
-      </div>
-      <div class="form-group col-sm btn-sm">
-        <button type="submit" id="btnsubmit" class="btn btn-info w-100 p-2" onclick="return check('btnsubmit', 'rolFerroEnvioLocalUpdate', '¡Alerta!', '¿Estás seguro quieres actualizar el registro?', 'info', 'Continuar', 'Cancelar', 'false');"><i class="fas fa-edit text-dark"></i> {{ __('Actualizar') }}</button>
-      </div>
-    </div>
     @include('global.paginador.paginador', ['paginar' => $envios])
   </div>
 </div>
