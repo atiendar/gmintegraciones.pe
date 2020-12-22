64c0c4d46a5c269887a02801ce3ce57a1282c58b
Aaron Sánchez <desarrolloweb.ewmx@gmail.com>
2020-12-21 19:50:29 -0600

    Mejora en los pagos

--- app/Http/Requests/pago/individual/StorePagoRequest.php
+++ app/Http/Requests/pago/individual/StorePagoRequest.php
@@ -16,7 +16,8 @@ class StorePagoRequest extends FormRequest {
 
     return [
       'comprobante_de_pago'                 => 'nullable|mimes:pdf,jpg,jpeg,png|max:1024',
-      'ultimos_8_digitos_del_folio_de_pago' => 'nullable|required_if:forma_de_pago,Cheque,Paypal,Tarjeta de credito (Pagina),Tarjeta de credito (Clip),Tarjeta de debito,Transferencia RUTH Yolanda,Transferencia Canastas y Arcones S.A de C.V|unique:pagos,fol|nullable|min:8|max:8',
+   //   'ultimos_8_digitos_del_folio_de_pago' => 'nullable|required_if:forma_de_pago,Cheque,Paypal,Tarjeta de credito (Pagina),Tarjeta de credito (Clip),Tarjeta de debito,Transferencia RUTH Yolanda,Transferencia Canastas y Arcones S.A de C.V|unique:pagos,fol|nullable|min:8|max:8',
+      'ultimos_8_digitos_del_folio_de_pago' => 'nullable|unique:pagos,fol|nullable|min:8|max:8',
       'forma_de_pago'                       => 'required|in:Cheque,Efectivo (Jonathan),Efectivo (Gabriel),Efectivo (Fernando),Paypal,Tarjeta de credito (Pagina),Tarjeta de credito (Clip),Tarjeta de debito,Transferencia RUTH Yolanda,Transferencia Canastas y Arcones S.A de C.V',
       'copia_de_identificacion'             => 'nullable|mimes:pdf,jpg,jpeg,png|max:1024',
       'monto_del_pago'                      => 'required|numeric|min:0|max:'.$max_monto.'|alpha_decimal15',
